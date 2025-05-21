76fb21a8788935f6db3dd0092a699c659bc9a9b7
ecisramust <eduardo.cisneros@ust.com>
2025-05-21 18:03:26 +0000

    step 12 of lab

---
+++ src/projects/ProjectList.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { Project } from './Project';
+import PropTypes from 'prop-types';
+import ProjectCard from './ProjectCard';
+import ProjectForm from './ProjectForm';
+
+function ProjectList({ projects }) {
+   
+  const [projectBeingEdited, setProjectBeingEdited] =useState();
+  const handleEdit = (project) => {
+    setProjectBeingEdited(project)  
+    //console.log(project);
+   };
+
+
+  const items = projects.map(project => (
+     <div key={project.id} className="cols-sm">
+       {project === projectBeingEdited ? (<ProjectForm/>): (<ProjectCard project={project} onEdit={handleEdit}/>)}
+      </div>
+    ));
+    return <div className="row">{items}</div>;
+}
+
+ProjectList.propTypes = {
+  projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired
+ };
+
+export default ProjectList;
