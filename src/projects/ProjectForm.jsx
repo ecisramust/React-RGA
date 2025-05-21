4136374d63ba986d44a20e708d2b787ae5b11058
ecisramust <eduardo.cisneros@ust.com>
2025-05-21 20:56:58 +0000

    form assignation fields, step lab 15

--- src/projects/ProjectForm.jsx
+++ src/projects/ProjectForm.jsx
@@ -1,24 +1,53 @@
-import PropTypes from 'prop-types';
+import PropTypes, { number } from 'prop-types';
 import { Project } from './Project';
+import { useState } from 'react';
 
-function ProjectForm({onCancel, onSave }) {
+function ProjectForm({onCancel, onSave, project: initialProject }) {
+
+  // important
+  const [project, setProject] = useState(initialProject);
 
   const handleSubmit = (event) =>{
     event.preventDefault();
-    onSave(new Project({name:  'Updated our Project'}));
+    onSave(project);
   };
+
+  const handleChange = (event) => {
+    const {type, name, value, checked} = event.target;
+    let updatedValue =  type === 'checkbox' ?  checked : value ;
+
+    // some validations
+    if (type === number){
+      updatedValue = Number(updatedValue);
+    }
+
+    const change = {
+      [name]: updatedValue,
+    };
+
+    let upatedProject;
+
+
+    setProject((p) => {
+      updatedProject = new Project({ ...p, ...change });
+      return updatedProject;
+    });
+
+
+  };
+
 
 
   return (
     <form className="input-group vertical" onSubmit={handleSubmit}>
       <label htmlFor="name">Project Name</label>
-      <input type="text" name="name" placeholder="enter name" />
+      <input type="text" name="name" placeholder="enter name" value={project.name}  onChange={handleChange}/>
       <label htmlFor="description">Project Description</label>
-      <textarea name="description" placeholder="enter description please" />
+      <textarea name="description" placeholder="enter description please"  value={project.description}  onChange={handleChange} />
       <label htmlFor="budget">Project Budget</label>
-      <input type="number" name="budget" placeholder="enter budget" />
+      <input type="number" name="budget" placeholder="enter budget"  value={project.budget}  onChange={handleChange}/>
       <label htmlFor="isActive">Active?</label>
-      <input type="checkbox" name="isActive" />
+      <input type="checkbox" name="isActive"  value={project.isActive}  onChange={handleChange}/>
       <div className="input-group">
         <button className="primary bordered medium">Save</button>
         <span />
@@ -32,6 +61,7 @@ function ProjectForm({onCancel, onSave }) {
 
 ProjectForm.propTypes = {
   onCancel: PropTypes.func.isRequired,
-  onSave: PropTypes.func.isRequired
+  onSave: PropTypes.func.isRequired,
+  project: PropTypes.instanceOf(Project)
 }
 export default ProjectForm;
--- src/projects/ProjectList.jsx
+++ src/projects/ProjectList.jsx
@@ -19,7 +19,7 @@ function ProjectList({ projects, onSave }) {
 
   const items = projects.map(project => (
      <div key={project.id} className="cols-sm">
-       {project === projectBeingEdited ? (<ProjectForm  onSave={onSave} onCancel={cancelEditing}/>): (<ProjectCard project={project} onEdit={handleEdit}/>)}
+       {project === projectBeingEdited ? (<ProjectForm project={project} onSave={onSave} onCancel={cancelEditing}/>): (<ProjectCard project={project} onEdit={handleEdit}/>)}
       </div>
     ));
     return <div className="row">{items}</div>;
